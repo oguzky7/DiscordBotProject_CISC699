a05a7d953a62bbb9070b0a9967149b420a85e2b3
oguzky7 <oguzky7@gmail.com>
2024-08-18 18:14:55 -0400

    Pushing Config File

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+
+class Config:
+    TOKEN = 'MTI2OTM4MTE4OTA1NjMzNTk3Mw.GffmcD.IIjtL9LYZInukDku1GBNtRoKkaH-5ntCP14HBk'
+    CHANNEL_ID = 1269383349278081054
+    KOSMOS_MAX_DATE = 'https://api.kosmosvize.com.tr/api/AdminDatas/getDatas?id=2329'
+    TRENDYOL_URL = 'https://www.trendyol.com/sr?q=English%20Home%20Mediterranean%20Kokulu%20%C3%87ubuk%20100%20ml&qt=English%20Home%20Mediterranean%20Kokulu%20%C3%87ubuk%20100%20ml&st=English%20Home%20Mediterranean%20Kokulu%20%C3%87ubuk%20100%20ml&os=1' 
+    
