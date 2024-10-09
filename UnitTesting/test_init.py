f7fba8d7574b8fbf9f9b3e5a20a81c2cba23c649
oguzky7 <oguzky7@gmail.com>
2024-10-09 11:17:07 -0400

    latest changes

--- UnitTesting/test_init.py
+++ UnitTesting/test_init.py
@@ -1,3 +1,13 @@
+"""
+test_init.py
+The primary objective is to consolidate all necessary imports in one place. 
+We avoid the redundancy of importing modules and dependencies repeatedly 
+in each test file. This helps streamline the test setup, making the individual 
+test files cleaner and easier to maintain, as they can focus purely on the logic 
+being tested rather than handling multiple import statements. This approach also 
+helps ensure consistency across all tests by having a single source for the 
+required libraries and modules.
+"""
 import sys, os, pytest, logging, asyncio
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from unittest.mock import patch, AsyncMock, MagicMock, Mock
@@ -12,3 +22,6 @@ from entity.DataExportEntity import ExportUtils
 from entity.PriceEntity import PriceEntity
 from entity.AvailabilityEntity import AvailabilityEntity
 from entity.EmailEntity import send_email_with_attachments
+
+if __name__ == "__main__":
+    pytest.main()
